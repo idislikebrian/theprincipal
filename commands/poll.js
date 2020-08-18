f00d6081e0f0ed4c7e508ba70396e82f6f589c76
Brian Felix <hello@brian-felix.com>
2020-08-18 19:42:26 -0400

    First commit. Hello!

---
+++ commands/poll.js
@@ -0,0 +1,28 @@
+const { Client, RichEmbed, MessageReaction } = require('discord.js');
+const bot = new Client();
+const Discord = require('discord.js');
+
+module.exports={
+    name: 'poll',
+    description: "This is a poll command.",
+    execute(message, args){
+        console.log(args)
+        if (args.length==0){
+            const Embed = new Discord.MessageEmbed()
+                .setColor(0xFFC300)
+                .setTitle("Looking to make a poll?")
+                .setDescription("Type *~poll {your question goes here}* to initiate a simple yes or no poll");
+               
+            message.channel.send(Embed);
+
+            } else { 
+            let msgArgs = args.slice(0).join(" ");
+
+            message.channel.send("📋 " + "**" + msgArgs + "**").then(MessageReaction => {
+                MessageReaction.react("👍");
+                MessageReaction.react("👎");
+                message.delete({ timeout: 5000, reason: 'It had to be done.' });
+            } )
+        }
+    }
+}
