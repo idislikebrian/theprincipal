174f175753e92ecc6bafcc763a2b983a0ef7b97f
Brian Felix <hello@brian-felix.com>
2020-08-24 22:22:41 -0400

    Adding a ~say command for FACILITATORS

---
+++ commands/say.js
@@ -0,0 +1,24 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+
+    if(!message.member.hasPermission(["MANAGE_MESSAGES", "ADMINISTRATOR"])) return message.channel.send("You're not allowed to use this command. I'm sorry!")
+
+    let argsresults; 
+    let mChannel = message.mentions.channels.first()
+    
+    message.delete()
+    if(mChannel) {
+        argsresults = args.slice(1).join(" ")
+        mChannel.send(argsresult)
+    } else {
+
+    }
+}
+
+
+module.exports.config = {
+    name: "say",
+    description: "sends a message that was inputted to a channel",
+    
+}
