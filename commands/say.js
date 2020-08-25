ef8b3b7c58a47c0f3cc11a8fb13bfd520481faa1
Brian Felix <hello@brian-felix.com>
2020-08-24 22:53:19 -0400

    20202408-2253

--- commands/say.js
+++ commands/say.js
@@ -2,27 +2,48 @@ const { Client, RichEmbed, MessageReaction } = require('discord.js');
 const bot = new Client();
 const Discord = require("discord.js");
 
-module.exports.config = {
-    name: "say",
-    description: "sends a message that was inputted to a channel",
-    usage: "say",
-    accessableby: "Staff"
-}
 
-module.exports.run = async (bot, message, args) => {
+// module.exports.run = async (bot, message, args) => {
 
-    if(!message.member.hasPermission(["MANAGE_MESSAGES", "ADMINISTRATOR"])) return message.channel.send("You're not allowed to use this command. I'm sorry!")
+//     if(!message.member.hasPermission(["MANAGE_MESSAGES", "ADMINISTRATOR"])) return message.channel.send("You're not allowed to use this command. I'm sorry!")
 
-    let argsresults; 
-    let mChannel = message.mentions.channels.first()
+//     let argsresults; 
+//     let mChannel = message.mentions.channels.first()
     
-    message.delete()
-    if(mChannel) {
-        argsresults = args.slice(1).join(" ")
-        mChannel.send(argsresult)
-    } else {
+//     message.delete()
+//     if(mChannel) {
+//         argsresults = args.slice(1).join(" ")
+//         mChannel.send(argsresult)
+//     } else {
 
+//     }
+// }
+
+
+// module.exports.config = {
+//     name: "say",
+//     description: "sends a message that was inputted to a channel",
+// }
+
+module.exports={
+    name: 'say',
+    description: "Sends a message that was inputter to a channel.",
+    execute(message, args){
+        console.log(args)
+        if (args.length==0){
+            const Embed = new Discord.MessageEmbed()
+                .setColor(0xFFC300)
+                .setTitle("Looking to make an announcement?")
+                .setDescription("Type *~say {your announcement goes here}* send an announcement");
+               
+            message.channel.send(Embed);
+
+            } else { 
+            let msgArgs = args.slice(0).join(" ");
+
+            message.channel.send("❗ " + "**" + msgArgs + "**").then(MessageReaction => {
+                message.delete({ timeout: 3500, reason: 'It had to be done.' });
+            } )
+        }
     }
 }
-
-
