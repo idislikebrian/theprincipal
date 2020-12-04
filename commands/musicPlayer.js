5ef886b46336bdbcf8fa104f64959e21cf1916dc
Brian Felix <hello@brian-felix.com>
2020-12-03 19:25:35 -0500

    Updating music player
    (thanks to iShibi)

--- commands/musicPlayer.js
+++ commands/musicPlayer.js
@@ -1,47 +1,38 @@
-const { Client, MessageEmbed, MessageReaction } = require('discord.js');
-const bot = new Client();
-const Discord = require("discord.js");
-
+const { MessageEmbed } = require('discord.js');
 const ytdl = require('ytdl-core');
 
-module.exports.execute = async (message) => {
-    // remove '!' and 'music' from the message content and get the video url
-    let musicLink = message.content.slice(1).split(' ').splice(1).shift();
-    let voiceChannel = message.member.voice.channel;
-
+module.exports = async (message) => {
+    const voiceChannel = message.member.voice.channel;
     if (voiceChannel) {
-        let urlValidity = ytdl.validateURL(musicLink);
-        if (urlValidity) {
-            const connection = await voiceChannel.join();
-            let dispatcher = connection.play(ytdl(musicLink, { filter: "audio" }));
+        const connection = await voiceChannel.join();
+        if (connection) {
+            const dispatcher = connection.play(ytdl(message.content, { format: 'audio' }));
+            const songInfo = await ytdl.getBasicInfo(message.content);
+            const musicEmbed = new MessageEmbed()
+                .setTitle('Music Player')
+                .setImage(`${songInfo.videoDetails.thumbnail.thumbnails[0].url}`)
+                .setFooter(`${songInfo.videoDetails.title}`)
+                .setColor('BLUE');
 
-            let deletedMessage = await message.delete();
-            let info = await ytdl.getBasicInfo(musicLink);
-
-            const musicPlayerEmbed = new MessageEmbed()
-                .setColor('BLUE')
-                .setTitle(info.videoDetails.title)
-                .setThumbnail(info.videoDetails.thumbnail.thumbnails[0].url);
-
-            let sentMusicPlayerEmbed = await deletedMessage.channel.send(musicPlayerEmbed);
-            sentMusicPlayerEmbed.react('⏹️');
-            sentMusicPlayerEmbed.react('⏸️');
+            const sentMusicEmbed = await message.channel.send(musicEmbed);
+            sentMusicEmbed.react('⏹️');
+            sentMusicEmbed.react('⏸️');
 
             let filter = (reaction, user) => !user.bot && user.id === message.author.id;
-            const reactionCollector = sentMusicPlayerEmbed.createReactionCollector(filter);
+            const reactionCollector = sentMusicEmbed.createReactionCollector(filter);
 
             reactionCollector.on('collect', (reaction) => {
                 if (reaction.emoji.name === '⏸️') {
                     dispatcher.pause();
                     reaction.remove();
-                    sentMusicPlayerEmbed.react('▶️');
+                    sentMusicEmbed.react('▶️');
                 } else if (reaction.emoji.name === '▶️') {
                     dispatcher.resume();
                     reaction.remove();
-                    sentMusicPlayerEmbed.react('⏸️');
+                    sentMusicEmbed.react('⏸️');
                 } else if (reaction.emoji.name === '⏹️') {
                     connection.disconnect();
-                    sentMusicPlayerEmbed.delete();
+                    sentMusicEmbed.delete();
                 } else {
                     reaction.remove();
                 }
@@ -50,13 +41,10 @@ module.exports.execute = async (message) => {
 
             dispatcher.on('finish', () => {
                 connection.disconnect();
-                sentMusicPlayerEmbed.delete();
+                sentMusicEmbed.delete();
             });
-
-        } else {
-            message.delete().then(m => m.channel.send('Provide a valid YouTube link so that I can play some music.'));
         }
     } else {
-        message.delete().then(m => m.channel.send('You need to join a voice channel first.'));
+        message.channel.send('Please join a voice channel first');
     }
-}
+};
