d2a6896266a46a56b4703728187f1711e332bd38
Brian Felix <hello@brian-felix.com>
2021-02-14 02:25:50 -0500

    Revert "Setting up queue feature"
    This reverts commit 328dc0121e69264d561e5b3266c98b9c178a522d.

--- commands/musicPlayer.js
+++ commands/musicPlayer.js
@@ -2,19 +2,14 @@ const { MessageEmbed } = require('discord.js');
 const ytdl = require('ytdl-core');
 const ytSearch = require('yt-search');
 
-const queue = new Map();
-
 module.exports={
     name: 'musicPlayer',
     description: "Helps you play music together",
     async execute (message, args){
         console.log(args)
-        const serverQueue = queue.get(message.guild.id);
         const voiceChannel = message.member.voice.channel;
         if (voiceChannel) {
             const connection = await voiceChannel.join();
-            if (!args.length) return message.channel.send('You need to send a link to a YouTube video as well.');
-            let song = {};
             if (connection) {
                 // searching for music via ytdl
                 if (ytdl.validateURL(args[0])) {
@@ -35,16 +30,14 @@ module.exports={
                 } 
                 const dispatcher = connection.play(ytdl(song.url, { format: 'audioonly' }));
                 const musicEmbed = new MessageEmbed()
-                    .setTitle('📻 Jukebox')
+                    .setTitle('Music Player')
                     .setImage(`${songInfo.videoDetails.thumbnail.thumbnails[0].url}`)
-                    .setFooter(`**Now Playing:** ${songInfo.videoDetails.title}`)
-                    .setTimestamp()
+                    .setFooter(`${songInfo.videoDetails.title}`)
                     .setColor('BLUE');
 
                 const sentMusicEmbed = await message.channel.send(musicEmbed);
                 sentMusicEmbed.react('⏹️');
                 sentMusicEmbed.react('⏸️');
-                sentMusicEmbed.react('⏭');
 
                 let filter = (reaction, user) => !user.bot && user.id === message.author.id;
                 const reactionCollector = sentMusicEmbed.createReactionCollector(filter);
@@ -59,19 +52,12 @@ module.exports={
                         reaction.remove();
                         sentMusicEmbed.react('⏸️');
                     } else if (reaction.emoji.name === '⏹️') {
-                        if (!message.member.voice.channel) return message.channel.send('You need to be in the voice channel where this music is playing to have a say.');
-                        serverQueue.songs = [];
-                        serverQueue.connection.disconnect();
+                        connection.disconnect();
                         sentMusicEmbed.delete();
-                    } else if (reaction.emoji.name == '⏭') {
-                        if (!message.member.voice.channel) return message.channel.send('You need to be in the voice channel where this music is playing to have a say.');
-                        if (!serverQueue){
-                            return message.channel.send(`There are no songs in the queue 😔`);
-                        }
-                        serverQueue.connection.dispatcher.end();
                     } else {
                         reaction.remove();
                     }
+
                 });
 
                 dispatcher.on('finish', () => {
@@ -84,20 +70,3 @@ module.exports={
         }
     }
 };
-/* working on dispatcher WIP WIP WIP WIP WIP
-const juke = async (guild, song) => {
-    const songQueue = queue.get(guild.id);
-
-    if (!song) {
-        songQueue.voiceChannel.leave();
-        queue.delete(guild.id);
-        return;
-    }
-    const stream = ytdl(song.url, {filter: 'audioonly' });
-    songQueue.connection.play(stream, { seek: 0, volume: 0.5})
-    .on('finish', () => {
-        songQueue.songs.shift();
-        juke(guild, songQueue.songs[0]);
-    });
-    await songQueue.text_channel.send(`🎶 Now Playing`)
-} */
