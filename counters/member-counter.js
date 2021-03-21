3244709fa266bf7d0115c15523b31bce683842e1
Brian Felix <hello@brian-felix.com>
2021-03-20 23:52:30 -0400

    Server stats
    More accurate member count, bot count and  patron count.

--- counters/member-counter.js
+++ counters/member-counter.js
@@ -1,12 +1,16 @@
 module.exports = async (client) =>{
     const guild = client.guilds.cache.get('708336363296260178');
     setInterval(() =>{
-        const memberCount = guild.memberCount;
+        const memberCount = guild.members.cache.filter(member => !member.user.bot).size;
         const botCount = guild.members.cache.filter(member => member.user.bot).size;
         const memChannel = guild.channels.cache.get('821155178400710686');
         const botChannel = guild.channels.cache.get('823031021582155797');
-        memChannel.setName(`👥 Member Count: ${memberCount.toLocaleString()}`)
-        botChannel.setName(`🤖 Bot Count: ${botCount.toLocaleString()}`)
+        const patrChannel = guild.channels.cache.get('823040693747843092')
+        let collectorRoleID = '823039147539038209';
+        const patronCount = guild.roles.get(collectorRoleID).members.size;
+        memChannel.setName(`👥 Members: ${memberCount.toLocaleString()}`)
+        botChannel.setName(`🤖 Bots: ${botCount.toLocaleString()}`)
+        patrChannel.setName(`💯 Patrons: ${patronCount.toLocaleString()}`)
         console.log('Updating Member Count');
     }, 60000);
 }
