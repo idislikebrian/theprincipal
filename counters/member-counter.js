0380704dad40ad07d64f16679b543df1736fa680
Brian Felix <hello@brian-felix.com>
2021-03-20 23:13:40 -0400

    Counting bots and increasing the time interval

--- counters/member-counter.js
+++ counters/member-counter.js
@@ -2,8 +2,11 @@ module.exports = async (client) =>{
     const guild = client.guilds.cache.get('708336363296260178');
     setInterval(() =>{
         const memberCount = guild.memberCount;
-        const channel = guild.channels.cache.get('821155178400710686');
-        channel.setName(`👥 Member Count: ${memberCount.toLocaleString()}`)
+        const botCount = guild.members.cache.filter(member => !member.user.bot).size;
+        const memChannel = guild.channels.cache.get('821155178400710686');
+        const botChannel = guild.channels.cache.get('823031021582155797');
+        memChannel.setName(`👥 Member Count: ${memberCount.toLocaleString()}`)
+        botChannel.setName(`🤖 Bot Count: ${botCount.toLocaleString()}`)
         console.log('Updating Member Count');
-    }, 5000);
+    }, 60000);
 }
