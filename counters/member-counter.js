4996e80475d506ac1ab08e81c105a8d0cd46fafe
Brian Felix <hello@brian-felix.com>
2021-03-21 00:48:34 -0400

    Patron counter

--- counters/member-counter.js
+++ counters/member-counter.js
@@ -7,7 +7,7 @@ module.exports = async (client) =>{
         const botChannel = guild.channels.cache.get('823031021582155797');
         const patrChannel = guild.channels.cache.get('823040693747843092')
         let collectorRoleID = '823039147539038209';
-        const patronCount = guild.roles.get(collectorRoleID).members.size;
+        const patronCount = guild.roles.cache.get(collectorRoleID).members.size;
         memChannel.setName(`👥 Members: ${memberCount.toLocaleString()}`)
         botChannel.setName(`🤖 Bots: ${botCount.toLocaleString()}`)
         patrChannel.setName(`💯 Patrons: ${patronCount.toLocaleString()}`)
