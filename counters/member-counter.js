155cd74dcc4aefd630b65b79966279c9e46c9a4c
Brian Felix <hello@brian-felix.com>
2021-03-20 23:40:02 -0400

    Bot counter

--- counters/member-counter.js
+++ counters/member-counter.js
@@ -2,7 +2,7 @@ module.exports = async (client) =>{
     const guild = client.guilds.cache.get('708336363296260178');
     setInterval(() =>{
         const memberCount = guild.memberCount;
-        const botCount = guild.members.cache.filter(member => !member.user.bot).size;
+        const botCount = guild.members.cache.filter(member => member.user.bot).size;
         const memChannel = guild.channels.cache.get('821155178400710686');
         const botChannel = guild.channels.cache.get('823031021582155797');
         memChannel.setName(`👥 Member Count: ${memberCount.toLocaleString()}`)
