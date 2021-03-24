6c72e37345421c3f024ed38e0cd09427c2b783e1
Brian Felix <hello@brian-felix.com>
2021-03-23 21:14:59 -0400

    Count new roles

--- counters/member-counter.js
+++ counters/member-counter.js
@@ -5,12 +5,20 @@ module.exports = async (client) =>{
         const botCount = guild.members.cache.filter(member => member.user.bot).size;
         const memChannel = guild.channels.cache.get('821155178400710686');
         const botChannel = guild.channels.cache.get('823031021582155797');
-        const patrChannel = guild.channels.cache.get('823040693747843092')
+        const patrChannel = guild.channels.cache.get('823040693747843092');
+        const staffChannel = guild.channels.cache.get('824015124356137060');
+        const modChannel = guild.channels.cache.get('824086983491584020')
         let collectorRoleID = '823039147539038209';
         const patronCount = guild.roles.cache.get(collectorRoleID).members.size;
+        let facilitatRoleID = '709389579651645502';
+        const modCount = guild.roles.cache.get(facilitatRoleID).members.size;
+        let atyStaffRoleID = '748232779807981698';
+        const staffCount = guild.roles.cache.get(atyStaffRoleID.member.size);
         memChannel.setName(`👥 Members: ${memberCount.toLocaleString()}`)
         botChannel.setName(`🤖 Bots: ${botCount.toLocaleString()}`)
         patrChannel.setName(`💯 Patrons: ${patronCount.toLocaleString()}`)
+        staffChannel.setName(`🎨 Staff: ${staffCount.toLocaleString()}`)
+        modChannel.setName(`😎 Facilitator: ${modCount.toLocaleString()}`)
         console.log('Updating Member Count');
     }, 60000);
 }
